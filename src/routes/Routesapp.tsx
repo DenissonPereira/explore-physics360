e6fcbef6f9387df9f50a6a8fca67b9d42298663e
Denisson Pereira Santos <denisson159@gmail.com>
2023-12-26 17:26:08 -0300

    up

--- src/routes/Routesapp.tsx
+++ src/routes/Routesapp.tsx
@@ -6,8 +6,8 @@ const Routesapp = () => {
   return (
     <HashRouter>
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
+            <Route path='/explore-physics360' element={<Home />} />
+            <Route path='/explore-physics360/about' element={<About />} />
         </Routes>
     </HashRouter>
   )

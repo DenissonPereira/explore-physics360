969cc7d81fcb68efb4b16304a3cdc581bab72324
Denisson Pereira Santos <denisson159@gmail.com>
2024-01-03 13:30:13 -0300

    Update Cal.tsx

--- src/calculadoras/Cal/Cal.tsx
+++ src/calculadoras/Cal/Cal.tsx
@@ -1,9 +1,29 @@
-
+import React from 'react';
 
 const Cal = () => {
-  return (
-    <div>Cal</div>
-  )
+    const display = document.getElementsByClassName('tela_numeros')[0];
+
+    function um() {
+        display.textContent = display.textContent + '1';
+    }
+
+    function dois() {
+        display.textContent = display.textContent + '2';
+    }
+
+    return (
+        <div className="calculadora_principal">
+            <div className="calc">
+                <div className="display">
+                    <div className="tela_numeros"></div>
+                </div>
+                <div className="teclas">
+                    <button id="1" onClick={um}>1</button>
+                    <button id="2" onClick={dois}>2</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Cal
+export default Cal;
